53734a7bf182bb87ed48a5b035976177b463c0db
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-09 01:54:14 -0800

    update travis.yml

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 
 # This is needed to login on AWS and push the image on ECR
 # Change it accordingly to your docker repo
-pip install awscli
+pipenv install awscli --system --ignore-pipfile
 export PATH=$PATH:$HOME/.local/bin
 eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
 
