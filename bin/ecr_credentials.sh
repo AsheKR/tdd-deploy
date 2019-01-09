c097639355fb13ea97ff45342472d131712fd2db
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-09 03:13:48 -0800

    update travis.yml

--- bin/ecr_credentials.sh
+++ bin/ecr_credentials.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOL
 [default]
-aws_access_key = $AWS_ACCESS_KEY
+aws_access_key_id = $AWS_ACCESS_KEY
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 EOL
 
